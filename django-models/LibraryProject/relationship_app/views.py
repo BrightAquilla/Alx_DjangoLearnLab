72eaf3f3fa9b7e62f68a01447c03896e3f6109c5
BrightAquilla <aquillabright@gmail.com>
2026-02-04 15:03:05 +0300

    Implement advanced Django model relationships, views, auth, and permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import user_passes_test, permission_required
 from .models import Book
 from .models import Library
