271290353598da99ffbe0296c0e7ab2422abb0e6
BrightAquilla <aquillabright@gmail.com>
2026-02-08 17:55:31 +0300

    templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.views.generic.detail import DetailView
 from .models import Library
 
 from django.shortcuts import render, redirect
 from .models import Book
+from .models import Author, Book, Library
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -63,19 +65,22 @@ def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 
-@permission_required('relationship_app.can_add_book')
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
-    pass
+    return render(request, 'relationship_app/add_book.html')
 
 
-@permission_required('relationship_app.can_change_book')
-def edit_book(request, pk):
-    pass
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    return render(request, 'relationship_app/edit_book.html')
 
 
-@permission_required('relationship_app.can_delete_book')
-def delete_book(request, pk):
-    pass
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    return render(request, 'relationship_app/delete_book.html')
+
 
 
 # Create your views here.
